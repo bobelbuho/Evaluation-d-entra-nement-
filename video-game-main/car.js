40d10320720398b2463887901916705a79988873
bobelbuho <bob.vaz@hotmail.fr>
2023-04-10 14:12:02 +0200

    car made

--- video-game-main/car.js
+++ video-game-main/car.js
@@ -5,4 +5,14 @@ class Car{
         this.width=width;
         this.height=height;
     }
+    draw(ctx){
+        ctx.beginPath();
+        ctx.rect(
+            this.x-this.width/2,
+            this.y-this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+    }
 }
