2bec8626e24ba3ff52e7e4f332c9cbb409c0b317
bobelbuho <bob.vaz@hotmail.fr>
2023-04-11 22:59:28 +0200

    car width fix

--- video-game-main/main.js
+++ video-game-main/main.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('myCanvas')
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 3, 50);
+const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
 animate();
