eaad4e5679a0bd9ff16002223ddcff14ec0ad393
bobelbuho <bob.vaz@hotmail.fr>
2023-04-12 19:09:38 +0200

    road

--- video-game-main/road.js
+++ video-game-main/road.js
@@ -9,5 +9,21 @@ class Road{
         this.right=x+width/2;
 
         const infinity= 10000000;
+        this.top=-infinity;
+        this.bottom=infinity;
+    }
+    draw(ctx){
+        ctx.lineWidth=5;
+        ctx.strokeStyle="white";
+
+        ctx.beginPath();
+        ctx.moveTo(this.left,this.top);
+        ctx.lineTo(this.left,this.bottom);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.right,this.top);
+        ctx.lineTo(this.right,this.bottom);
+        ctx.stroke();
     }
 }
