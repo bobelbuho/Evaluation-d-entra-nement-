9f3489aab853d5e8c610931ce8c7598e28afb7e4
bobelbuho <bob.vaz@hotmail.fr>
2023-04-15 12:54:57 +0200

    borders fix

--- video-game-main/road.js
+++ video-game-main/road.js
@@ -12,10 +12,10 @@ class Road {
         this.top = -infinity;
         this.bottom = infinity;
 
-        const topLeft=(x:this.left,y:this.top);
-        const topRight=(x:this.right,y:this.top);
-        const bottomLeft=(x:this.left,y:this.bottom);
-        const bottomRight=(x:this.right,y:this.bottom);
+        const topLeft={x:this.left,y:this.top};
+        const topRight={x:this.right,y:this.top};
+        const bottomLeft={x:this.left,y:this.bottom};
+        const bottomRight={x:this.right,y:this.bottom};
         this.borders=[
             [topLeft,bottomLeft],
             [topRight, bottomRight]
