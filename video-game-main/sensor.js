ee0f99aaf45461791cf9a3e338e364f3af2f6c0b
bobelbuho <bob.vaz@hotmail.fr>
2023-04-19 12:10:15 +0200

    56.41

--- video-game-main/sensor.js
+++ video-game-main/sensor.js
@@ -33,6 +33,10 @@ class Sensor{
             ctx.moveTo(
                 this.rays[i][0].x,
                 this.rays[i][0].y
+            );
+            ctx.lineTo(
+                this.rays[i][1].x,
+                this.rays[i][1].y
             )
         }
     }
